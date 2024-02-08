6c6582e358d3f0ba2a85a0fb689f08720da57db7
Saki Reid <saki.reid@gmail.com>
2024-02-08 14:41:44 +0100

    removing html body

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -4,27 +4,25 @@ export const NavBar = () => {
   const { pathname } = useLocation();
 
   return (
-    <body>
-      <header>
-        <div className="flex justify-between items-center landing-page-info">
-          <NavLink to="/" className="nav-bar__logo_container flex ">
-            <div className="flex ">
-              <h1 className="font-thin antialiased md:ml-4 lowercase tracking-wider text-6xl	 ">
-                Saki Reid
-              </h1>
-            </div>
-          </NavLink>
-          <div className="nav-bar__nav-items flex font-thin flex-col text-right tracking-wider">
-            <NavLink to="/portfolio">Work</NavLink>
-            <NavLink to="portfolio#about">About</NavLink>
-            <NavLink to="portfolio#contact">Contact</NavLink>
+    <header>
+      <div className="flex justify-between items-center landing-page-info">
+        <NavLink to="/" className="nav-bar__logo_container flex ">
+          <div className="flex ">
+            <h1 className="font-thin antialiased md:ml-4 lowercase tracking-wider text-6xl	 ">
+              Saki Reid
+            </h1>
           </div>
+        </NavLink>
+        <div className="nav-bar__nav-items flex font-thin flex-col text-right tracking-wider">
+          <NavLink to="/portfolio">Work</NavLink>
+          <NavLink to="portfolio#about">About</NavLink>
+          <NavLink to="portfolio#contact">Contact</NavLink>
         </div>
-        <nav>
-          <NavLink className="nav-bar__logo_container" to="/"></NavLink>
-        </nav>
-      </header>
-    </body>
+      </div>
+      <nav>
+        <NavLink className="nav-bar__logo_container" to="/"></NavLink>
+      </nav>
+    </header>
   );
 };
 
